dbfbc0e07186340c9938466eaeff7dc8c1c5d5ac
WangYuTengg <wyuteng123@gmail.com>
2025-02-18 22:11:59 +0800

    fix path

--- components/BaseAPI.js
+++ components/BaseAPI.js
@@ -16,7 +16,7 @@ export var methodType = {get : 'GET', post : 'POST', put : 'PUT', delete : 'DELE
 
 export default class BaseAPI {	
 	static JSONRequest(api, method, headers, options, content) {
-		const host = "https://mdpapi.kyaw.tech/";
+		const host = "https://mdpapi.kyaw.tech";
 
 		let requestOptions = {
 			method: method,
